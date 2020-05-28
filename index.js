26aae7e699a4e0107fb7659582cc815e4dc5ae6d
Bart079 <64658493+Bart079@users.noreply.github.com>
2020-05-28 13:29:02 +0200

    init

--- index.js
+++ index.js
@@ -108,83 +108,79 @@ bot.on("message", async message => {
     }
 })  
     
-    if (cmd === `${prefix}kick`) {
-        //kick @spelerNaam redenen hier
-        
-        var argument = message.content.slice(prefix.length).split(/ +/);
-
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("you can not use that command")
-
-        if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("you dont have premissions to do that");
+if (cmd === `${prefix}kick`) {
  
-        if (!argument[1]) return message.reply("No user given.");
- 
-        if (!argument[2]) return message.reply("Please give a reason.");
- 
-        var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(argument[1]));
+    const args = message.content.slice(prefix.length).split(/ +/);
 
-        var reason = argument.slice(2).join(" ");
- 
-        if (!kickUser) return message.reply("No user found.");
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry jij kan dit niet");
 
-        const Discord = require('discord.js')
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("Geen perms");
 
-        var embed = new Discord.MessageEmbed()
+    if (!args[1]) return message.reply("Geen gebruiker opgegeven.");
+
+    if (!args[2]) return message.reply("Gelieve een redenen op te geven.");
+
+    var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+
+    var reason = args.slice(2).join(" ");
+
+    if (!kickUser) return message.reply("Kan de gebruiker niet vinden.");
+
+    var embed = new discord.MessageEmbed()
         .setColor("#ff0000")
-        .setThumbnail(kickUser.user.displayAvatarURL())
-        .setFooter(message.member.displayName, message.author.displayAvatarURL())
+        .setThumbnail(kickUser.user.displayAvatarURL)
+        .setFooter(message.member.displayName, message.author.displayAvatarURL)
         .setTimestamp()
-        .setDescription(`** Kicked:** ${kickUser} (${kickUser.id})
-        **Kicked by:** ${message.author}
-        **Reason: ** ${reason}`);
+        .setDescription(`** Gekickt:** ${kickUser} (${kickUser.id})
+        **Gekickt door:** ${message.author}
+        **Redenen: ** ${reason}`);
 
-        var embedPrompt = new Discord.MessageEmbed()
-        .setColor("groen")
-        .setAuthor("Please react in 30 sec.")
-        .setDescription(`Are you sure you want to kick ${kickUser}?`); 
-
-
-        var embed1 = new Discord.MessageEmbed()
-        .setColor("red")
+    var embedPrompt = new discord.MessageEmbed()
+        .setColor("GREEN")
+        .setAuthor("Gelieve te reageren binnen 30 sec.")
+        .setDescription(`Wil je ${kickUser} kicken?`);
 
 
-        message.channel.send(embedPrompt).then(async msg => {
+    message.channel.send(embedPrompt).then(async msg => {
 
         var emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
+
+
         // We kijken dat het de gebruiker is die het als eerste heeft uitgevoerd.
-            // message.channel.awaitMessages(m => m.author.id == message.author.id,
-            //     { max: 1, time: 30000 }).then(collected => {
- 
-            //         if (collected.first().content.toLowerCase() == 'yes') {
-            //             message.reply('Kick speler.');
-            //         }
-            //         else
-            //             message.reply('Geanuleerd');
- 
-            //     }).catch(() => {
-            //         message.reply('Geen antwoord na 30 sec, geanuleerd.');
-            //     });
- 
- 
-            if (emoji === "✅") {
- 
-                msg.delete();
- 
-                kickUser.kick(reason).catch(err => {
-                    if (err) return message.channel.send(`Something went wrong.`);
-                });
- 
-                message.channel.send(embed);
- 
-            } else if (emoji === "❌") {
- 
-                msg.delete();
- 
-                message.reply("Kick Canceld").then(m => m.delete(5000));
- 
-            }
-        });
-    } 
+        // message.channel.awaitMessages(m => m.author.id == message.author.id,
+        //     { max: 1, time: 30000 }).then(collected => {
+
+        //         if (collected.first().content.toLowerCase() == 'yes') {
+        //             message.reply('Kick speler.');
+        //         }
+        //         else
+        //             message.reply('Geanuleerd');
+
+        //     }).catch(() => {
+        //         message.reply('Geen antwoord na 30 sec, geanuleerd.');
+        //     });
+
+
+        if (emoji === "✅") {
+
+            msg.delete();
+
+            kickUser.kick(reason).catch(err => {
+                if (err) return message.channel.send(`Er is iets foutgegaan.`);
+            });
+
+            message.reply(embed);
+
+        } else if (emoji === "❌") {
+
+            msg.delete();
+
+            message.reply("Kick geanuleerd").then(m => m.delete(5000));
+
+        }
+
+    });
+}
 
 
 
