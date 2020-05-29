eec5c5e3981c6648a9821f585fbf5843ee6cd879
Bart079 <64658493+Bart079@users.noreply.github.com>
2020-05-29 15:06:19 +0200

    init

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ bot.on("message", async message => {
                 let embed = new Discord.MessageEmbed()
                     .setTitle("cute cats")
                     .setColor(colors.rose)
-                    .setImage(JSON.iconURL)
+                    .setImage(json.iconURL)
                     .setFooter(`Server: God coding |`, bot.user.displayAvatarURL());
                 msg.edit(embed)
             });
